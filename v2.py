2fc4328534d49205f4c45344cf95625d6a58c40c
fmsol94 <61605129+fmsol94@users.noreply.github.com>
2023-05-08 18:18:09 +0200

    blocks implemented

--- v2.py
+++ v2.py
@@ -110,13 +110,19 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
     
-# class Block(nn.Module):
-#     def __init__(self, n_embd, n_head):
-#         # n_embd: Embedding dimension, n_head: the number of heads 
-#         super().__init__()
-#         head_size = n_embd//n_head
-#         self.sa = MultiHeadAttention(n_head, head_size)
-#         self.ffwd = Feed
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+    def __init__(self, n_embd, n_head):
+        # n_embd: Embedding dimension, n_head: the number of heads 
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.ffwd(x)
+        return x
 
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -124,8 +130,11 @@ class BigramLanguageModel(nn.Module):
         # Each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention. It will be concatenated into a 32-dimensional array, same as n_embd
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -134,8 +143,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_heads(x) # Apply one head of self-attention
-        x = self.ffwd(x) # (B, T, C)
+        x = self.blocks(x) # (B, T, C)
         logits = self.lm_head(x) #(B, T, vocab_size)
         if targets is None:
             loss = None
